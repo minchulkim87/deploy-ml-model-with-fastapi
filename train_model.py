a248f65ac849176b05b35b42b0fb181235340e41
Min Kim <minchulkim87@gmail.com>
2022-11-12 21:57:45 +1100

    added docstrings

--- train_model.py
+++ train_model.py
@@ -1,4 +1,7 @@
+"""
 # Script to train machine learning model.
+"""
+
 import logging
 import numpy as np
 import pandas as pd
@@ -8,7 +11,7 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import fbeta_score
 import joblib
 
-from typing import List, Optional
+from typing import List, Tuple, Optional
 
 
 CAT_FEATURES = [
@@ -23,7 +26,12 @@ CAT_FEATURES = [
 ]
 
 
-def load_data():
+def load_data() -> pd.DataFrame:
+    """Loads raw data
+
+    Returns:
+        pd.DataFrame: raw data
+    """
     return pd.read_csv("data/census.csv", skipinitialspace=True, low_memory=False)
 
 
@@ -32,17 +40,30 @@ def process_data(data: pd.DataFrame,
                  train: bool=True,
                  categorical_features: Optional[List[str]]=None,
                  encoder: Optional[OneHotEncoder]=None,
-                 lb: Optional[LabelBinarizer]=None):
+                 lb: Optional[LabelBinarizer]=None) -> Tuple[np.ndarray, np.ndarray, OneHotEncoder, LabelBinarizer]:
+    """processes raw data and converts categorical columns and target labels into sklearn-ready types.
+
+    Args:
+        data (pd.DataFrame): raw data
+        label (str): column name of the target variable
+        train (bool, optional): whether to create encoder and binarizer or not. Defaults to True.
+        categorical_features (Optional[List[str]], optional): list of columns to treat as categorical. Defaults to None.
+        encoder (Optional[OneHotEncoder], optional): if train is False, and there are categorical_features, provide a OneHotEncoder. Defaults to None.
+        lb (Optional[LabelBinarizer], optional): if train is False, provide a LabelBinarizer. Defaults to None.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray, OneHotEncoder, LabelBinarizer]: X, y, onehotencoder, label_binarizer
+    """
     
     if train:
-        binarizer = LabelBinarizer()
+        label_binarizer = LabelBinarizer()
         onehotencoder = OneHotEncoder(sparse=False, handle_unknown="ignore")
     else:
-        binarizer = lb
+        label_binarizer = lb
         onehotencoder = encoder
     
     y = data.pop(label)
-    y = binarizer.fit_transform(y.values).ravel()
+    y = label_binarizer.fit_transform(y.values).ravel()
     
     if categorical_features:
         X = np.concatenate(
@@ -56,17 +77,30 @@ def process_data(data: pd.DataFrame,
     else:
         X= data
     
-    return X, y, onehotencoder, binarizer
+    return X, y, onehotencoder, label_binarizer
 
 
-def save_processed_data(X_train, X_test, y_train, y_test):
+def save_processed_data(X_train: np.ndarray, X_test: np.ndarray, y_train: np.ndarray, y_test: np.ndarray) -> None:
+    """Saves processed data
+
+    Args:
+        X_train (np.ndarray): X train
+        X_test (np.ndarray): X test
+        y_train (np.ndarray): y train
+        y_test (np.ndarray): y test
+    """
     pd.DataFrame(X_train).to_csv("data/X_train.csv", index=False, encoding="utf-8")
     pd.DataFrame(X_test).to_csv("data/X_test.csv", index=False, encoding="utf-8")
     pd.Series(y_train).to_csv("data/y_train.csv", index=False, encoding="utf-8")
     pd.Series(y_test).to_csv("data/y_test.csv", index=False, encoding="utf-8")
 
 
-def load_processed_data():
+def load_processed_data() -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """Loads pre-processed data
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: X_train, X_test, y_train, y_test
+    """
     X_train = pd.read_csv("data/X_train.csv").values
     X_test = pd.read_csv("data/X_test.csv").values
     y_train = pd.read_csv("data/y_train.csv").values
@@ -74,26 +108,56 @@ def load_processed_data():
     return X_train, X_test, y_train, y_test
 
 
-def train_model(X, y):
+def train_model(X: np.ndarray, y: np.ndarray) -> AdaBoostClassifier:
+    """Trains an adaBoost model using the given data and returns the classifier.
+
+    Args:
+        X (np.ndarray): X train
+        y (np.ndarray): y train
+
+    Returns:
+        AdaBoostClassifier: classifier
+    """
     model = AdaBoostClassifier()
     model.fit(X, y)
     return model
 
 
-def save_model_artifacts(model, encoder, lb):
+def save_model_artifacts(model: AdaBoostClassifier, encoder: OneHotEncoder, lb: LabelBinarizer) -> None:
+    """Saves model artifacts as pickles
+
+    Args:
+        model (AdaBoostClassifier): trained model
+        encoder (OneHotEncoder): trained one hot encoder for categorical variables
+        lb (LabelBinarizer): trained label binarizer for target variable
+    """
     joblib.dump(model, 'model/model.pkl')
     joblib.dump(encoder, 'model/encoder.pkl')
     joblib.dump(lb, 'model/lb.pkl')
 
 
-def load_model_artifacts():
+def load_model_artifacts() -> Tuple[AdaBoostClassifier, OneHotEncoder, LabelBinarizer]:
+    """Loads the saved artifacts
+
+    Returns:
+        Tuple[AdaBoostClassifier, OneHotEncoder, LabelBinarizer]: model, encoder, lb
+    """
     model = joblib.load('model/model.pkl')
     encoder = joblib.load('model/encoder.pkl')
     lb = joblib.load('model/lb.pkl')
     return model, encoder, lb
 
 
-def compute_model_metrics(y, pred):
+def compute_model_metrics(y: np.ndarray, pred: np.ndarray) -> float:
+    """Computes and returns f1-score
+
+    Args:
+        y (np.ndarray): true labels
+        pred (np.ndarray): model predicted labels
+
+    Returns:
+        float: f1-score
+    """
     return fbeta_score(y, pred, beta=1, zero_division=1)
 
 
